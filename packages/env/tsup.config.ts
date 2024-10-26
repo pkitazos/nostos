782b5f5e5e859636c80a4abae02d573968a58736
Petros <93052465+pkitazos@users.noreply.github.com>
2024-10-26 01:11:01 +0100

    fixed env package tsup config

---
+++ packages/env/tsup.config.ts
@@ -0,0 +1,9 @@
+// packages/env/tsup.config.ts
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["esm", "cjs"],
+  dts: true,
+  clean: true,
+});
