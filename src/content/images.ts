c2261d8c2e95cea0aa24d989fd49d898eb17ed90
Petros <93052465+pkitazos@users.noreply.github.com>
2025-01-27 12:00:15 +0000

    added downsized images

--- src/content/images.ts
+++ src/content/images.ts
@@ -1,82 +1,82 @@
 export const SITE_IMAGES = {
-    home:{
-        heroBanner: {
-            src: "/site/hero-banner.jpg",
-            width: 4231,
-            height: 6346
-        },
-        ourApproach: {
-            src: "/site/home-our-approach.jpg",
-            width: 5384,
-            height: 8076
-        },
-        banner1:{
-            src: "/site/home-banner-1.jpg",
-            width: 8126,
-            height: 5420
-        },
-        banner2:{
-            src: "/site/home-banner-2.jpg",
-            width: 5384,
-            height: 8076
-        },
+  home: {
+    heroBanner: {
+      src: '/site/hero-banner.jpg',
+      width: 4231,
+      height: 6346,
     },
-    ourPhilosophy:{
-        heroBanner:{
-            src: "/site/our-philosophy-hero-banner.jpg",
-            width: 4416,
-            height: 6624
-        },
+    ourApproach: {
+      src: '/site/home-our-approach.jpg',
+      width: 5384,
+      height: 8076,
     },
-    services:{
-        img1:{
-            src: "/site/services/037A4206-132.jpg",
-            width: 4416,
-            height: 6624
-        },
-        img2:{
-            src: "/site/services/037A4225-140.jpg",
-            width: 4416,
-            height: 6624
-        },
-        img3:{
-            src: "/site/services/037A4211-134.jpg",
-            width: 4416,
-            height: 6624
-        },
-        img4:{
-            src: "/site/services/037A4214-136.jpg",
-            width: 4416,
-            height: 6624
-        },
-        img5:{
-            src: "/site/services/037A4315-150.jpg",
-            width: 4416,
-            height: 6624
-        },
-        img6:{
-            src: "/site/services/037A4252-144.jpg",
-            width: 4416,
-            height: 6624
-        },
+    banner1: {
+      src: '/site/home-banner-1.jpg',
+      width: 8126,
+      height: 5420,
     },
-    contactUs:{
-        heroBanner:{
-            src: "/site/contact-us-hero-banner.jpg",
-            width: 5384,
-            height: 8076
-        },
+    banner2: {
+      src: '/site/home-banner-2.jpg',
+      width: 5384,
+      height: 8076,
     },
-    gallery:{
-        photosButton:{
-            src: "/site/gallery-photos.jpg",
-            width: 5384,
-            height: 8076
-        },
-        videosButton:{
-            src: "/site/gallery-videos.jpg",
-            width: 5384,
-            height: 8076
-        },
-    }
+  },
+  ourPhilosophy: {
+    heroBanner: {
+      src: '/site/our-philosophy-hero-banner.jpg',
+      width: 4416,
+      height: 6624,
+    },
+  },
+  services: {
+    img1: {
+      src: '/site/services/img-1.jpg',
+      width: 4416,
+      height: 6624,
+    },
+    img2: {
+      src: '/site/services/img-2.jpg',
+      width: 4416,
+      height: 6624,
+    },
+    img3: {
+      src: '/site/services/img-3.jpg',
+      width: 4416,
+      height: 6624,
+    },
+    img4: {
+      src: '/site/services/img-4.jpg',
+      width: 4416,
+      height: 6624,
+    },
+    img5: {
+      src: '/site/services/img-5.jpg',
+      width: 4416,
+      height: 6624,
+    },
+    img6: {
+      src: '/site/services/img-6.jpg',
+      width: 4416,
+      height: 6624,
+    },
+  },
+  contactUs: {
+    heroBanner: {
+      src: '/site/contact-us-hero-banner.jpg',
+      width: 5384,
+      height: 8076,
+    },
+  },
+  gallery: {
+    photosButton: {
+      src: '/site/gallery-photos.jpg',
+      width: 5384,
+      height: 8076,
+    },
+    videosButton: {
+      src: '/site/gallery-videos.jpg',
+      width: 5384,
+      height: 8076,
+    },
+  },
 }
